50038e8de93635a6f1ac2fe3e445bcb7346d2552
gstache <gstache2@gmail.com>
2015-09-22 14:55:44 -0400

    created possessible NP
    has methods possess() and dePossess() that only work when a game object
    tagged "Player" call them, and then they parent the NPC to the player
    object and disable the player mesh renderer so that only the NPC can be
    seen. to test, press P to possess, WASD to move

---
+++ Assets/Scripts/Possessible.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Possessible : MonoBehaviour {
+	GameObject player;
+	bool possessable;
+	bool isPossessed = false;
+	int possDelay;
+	// Use this for initialization
+	void Start () {
+		possDelay = 0;
+		possessable = false;
+	}
+
+	void Update () { 
+		if (isPossessed) {
+			transform.position = player.transform.position;
+			possDelay+=1;
+		}
+	}
+
+	void OnTriggerEnter(Collider target){
+		if (target.gameObject.tag.Equals ("Player")) {
+			player = target.gameObject;
+			possessable = true;
+		}
+	}
+
+	void OnTriggerExit(){
+		possessable = false;
+	}
+
+	public void possess(){
+		if (possessable) {
+
+				isPossessed = true;
+			gameObject.GetComponent<CapsuleCollider>().enabled = false;
+			gameObject.GetComponent<Rigidbody>().useGravity = false;
+			transform.position = player.transform.position; 
+			this.transform.SetParent (player.transform);
+			player.GetComponent<MeshRenderer> ().enabled = false;
+		}
+	}
+
+	public void dePossess(){
+		if (possDelay > 10) {
+			player.GetComponent<MeshRenderer> ().enabled = true;
+			isPossessed = false;
+			this.transform.SetParent (null);
+			transform.Translate (new Vector3 (1, 0, 1));
+			gameObject.GetComponent<CapsuleCollider> ().enabled = true;
+			gameObject.GetComponent<Rigidbody> ().useGravity = true;
+			possDelay = 0;
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/tempController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tempController : MonoBehaviour {
+	private bool poss;
+	GameObject npc;
+	// Use this for initialization
+	void Start () {
+		poss = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxisRaw("Horizontal") != 0){
+			gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(Input.GetAxisRaw("Horizontal")*30,0,0));
+		} 
+		if(Input.GetAxisRaw("Vertical") != 0){
+			gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0,0,Input.GetAxisRaw("Vertical")*30));
+		}
+
+		if (Input.GetKey ("p")) {
+
+
+			possess ();
+		}
+
+	}
+
+	void OnTriggerEnter(Collider target){
+		if (target.tag.Equals ("NPC")) {
+			npc = target.gameObject;
+		}
+	}
+
+	void OnTriggerExit(){
+		npc = null;
+	}
+
+	void possess(){
+		if (poss && npc) {
+			npc.GetComponent<Possessible> ().dePossess ();
+		
+			poss = !poss;
+		} else if (!poss && npc) {
+			npc.GetComponent<Possessible> ().possess ();
+
+			poss = !poss;
+		}
+	}
+	IEnumerator Example() {
+		yield return new WaitForSeconds(2);
+	}
+}
