39cc8ec45993adf98bda8b854b5ea0633fcf99d5
gstache <gstache2@gmail.com>
2015-10-15 10:06:31 -0400

    added milestone 2 project

---
+++ Milestone_2/Assets/GameManager.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public enum MODE{text, image};
+	GameObject[] furniture;
+	public Text timerUI;
+	public Image blinkUI;
+	public MODE mode;
+	private float currentPosTime;
+	public float posessionTime;
+	public float outOfBodyTime;
+	public GameObject player;
+	GhostScript ghost;
+	private float timer;
+	private bool blinkRecover;
+	public float flashSpeed;
+	private float blinkTimer;
+	private float resetTimer;
+	bool wasPossessing;
+	// Use this for initialization
+	void Start () {
+		ghost = player.GetComponent<GhostScript> ();
+		wasPossessing = false;
+		currentPosTime = outOfBodyTime;
+		blinkTimer = 0;
+		furniture = GameObject.FindGameObjectsWithTag ("Chair");
+		disablePhysics ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Alpha1)){
+			mode = MODE.text;
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha2)){
+			mode = MODE.image;
+		}
+		if (!wasPossessing && ghost.poss) {
+			wasPossessing = ghost.poss;
+			currentPosTime = posessionTime;
+			blinkTimer = 0;
+			activatePhysics();
+		} else if (wasPossessing && !ghost.poss) {
+			wasPossessing = ghost.poss;
+			currentPosTime = outOfBodyTime;
+			blinkTimer = 0;
+			disablePhysics();
+		}
+		if(currentPosTime > 0){
+			resetTimer = 0;
+
+			currentPosTime -= Time.deltaTime;
+			if(mode == MODE.text){
+				timerUI.enabled = true;
+				blinkUI.enabled = false;
+				timerUI.text = "Time: " + currentPosTime;
+			}
+			else if(mode == MODE.image){
+				timerUI.text = "";
+				blinkUI.color = new Vector4(1,1,1, Mathf.Min(.7f,((blinkTimer*.5f) / currentPosTime)*.2f));
+			}
+		}
+		else{
+			Reset();
+		}
+
+		blinkTimer += Time.deltaTime;
+
+	}
+
+	void Reset(){
+		Debug.Log ("Resetting");
+		timerUI.text = "Time: 0";
+		if (ghost.poss) {
+			ghost.npc.GetComponent<Possessible> ().dePossess ();
+		} else {
+			Application.LoadLevel ("room_for_wake");
+		}
+	}
+	void activatePhysics(){
+		foreach (GameObject chair in furniture) {
+			chair.GetComponent<Rigidbody> ().isKinematic = false;
+			chair.GetComponent<Collider> ().enabled = true;
+		}
+	}
+
+	void disablePhysics(){
+		foreach (GameObject chair in furniture) {
+			chair.GetComponent<Rigidbody> ().isKinematic = true;
+			chair.GetComponent<Collider> ().enabled = false;
+		}
+	}
+
+	void Blink(){
+		blinkUI.color = new Vector4(1,1,1,.5f);
+		blinkRecover = true;
+		blinkTimer = 0;
+	}
+
+	void EndBlink(){
+		blinkTimer += Time.deltaTime;
+		flashSpeed = 3f - currentPosTime / 10f;
+		blinkUI.color = Color.Lerp( new Vector4(1,1,1,.5f), Color.clear, flashSpeed * blinkTimer);
+		if(blinkUI.color == Color.clear){
+			blinkRecover = false;
+		}
+	}
+	
+}
---
+++ Milestone_2/Assets/Scripts/Possessible.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Possessible : MonoBehaviour {
+	GameObject player;
+	bool possessable;
+	bool isPossessed = false;
+	int possDelay;
+	Animator anim;
+	public Component[] boneRig;
+	public int ReviveTime;
+	int time;
+	// Use this for initialization
+	void Start () {
+		possDelay = 0;
+		possessable = false;
+		anim = GetComponent<Animator> ();
+		boneRig = gameObject.GetComponentsInChildren <Rigidbody>();
+	}
+
+	void Update () { 
+		if (isPossessed) {
+			player.transform.position = transform.position;
+			player.transform.rotation = transform.rotation;
+			if (Input.GetAxisRaw ("Vertical") > 0) {
+				anim.SetInteger ("AnimState", 1);
+			} else if (Input.GetAxisRaw ("Vertical") < 0) {
+				anim.SetInteger ("AnimState", -1);
+			} else {
+				anim.SetInteger ("AnimState", 0);
+			}
+			if (Input.GetAxisRaw ("Horizontal") != 0) {
+				transform.Rotate (0, Input.GetAxisRaw ("Horizontal") * 80.0f * Time.deltaTime, 0, Space.World);
+			}
+			possDelay += 1;
+		} else {
+			anim.SetInteger ("AnimState", 0);
+		}
+	
+	}
+
+	void OnTriggerEnter(Collider target){
+		if (target.gameObject.tag.Equals ("Player")) {
+			Debug.Log ("player Enters");
+			player = target.gameObject;
+			possessable = true;
+		}
+	}
+
+	void OnTriggerExit(){
+		//possessable = false;
+	}
+
+	public void possess(){
+
+		if (possessable && !player.GetComponent<GhostScript>().poss) {
+
+			player.gameObject.GetComponent<CapsuleCollider>().enabled = false;
+			player.gameObject.GetComponent<Rigidbody>().useGravity = false;
+			transform.position = player.transform.position; 
+			//player.transform.SetParent (this.transform);
+			player.GetComponentInChildren<SkinnedMeshRenderer> ().enabled = false;
+			isPossessed = true;
+			player.GetComponent<GhostScript>().poss = true;
+
+		}
+	}
+
+	public void dePossess(){
+		if (possDelay > 10) {
+			Kill ();
+			player.GetComponentInChildren<SkinnedMeshRenderer> ().enabled = true;
+			isPossessed = false;
+
+
+			player.GetComponent<GhostScript>().poss = false;
+			possDelay = 0;
+
+			player.gameObject.GetComponent<CapsuleCollider> ().enabled = true;
+			player.gameObject.GetComponent<Rigidbody> ().useGravity = true;
+			possessable = false;
+		}
+	}
+
+	public void Kill(){
+		foreach (Rigidbody ragbone in boneRig) {
+			ragbone.isKinematic = false;
+		}
+		GetComponent<Animator> ().enabled = false;
+		time = 0;
+	}
+
+	void Revive(){
+		foreach (Rigidbody ragbone in boneRig) {
+			ragbone.isKinematic = true;
+		}
+		GetComponent<Animator> ().enabled = true;
+		possessable = true;
+	}
+
+}
