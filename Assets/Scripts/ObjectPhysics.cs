7be20e7369c0a47c76f1b40b64b33c47f8a3f0fb
Sasha Azad <sazad6@gatech.edu>
2015-10-16 00:44:30 -0400

    pushing box working - dungeon - m2 sasha

---
+++ Assets/Scripts/ObjectPhysics.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectPhysics : MonoBehaviour {
+
+	void OnControllerColliderHit(ControllerColliderHit hit){
+
+		float pushPower = 1.0f; 
+		float weight = 6.0f;
+		Vector3 force;
+		Rigidbody body = hit.collider.attachedRigidbody;
+
+		if (body == null || body.isKinematic) {
+			return;
+		}
+
+		if (hit.moveDirection.y < -0.3) {
+			return;
+		}
+
+		force = hit.controller.velocity * pushPower;
+		Vector3 pushDir = new Vector3 (hit.moveDirection.x, 0, hit.moveDirection.z) * pushPower;
+
+		body.velocity = pushDir;
+//		body.AddForceAtPosition (force, hit.point);
+
+	
+	}
+
+}
