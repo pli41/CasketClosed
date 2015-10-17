1718a655ecf7aaa19b338499711500ad487b9c0d
Tiff Zhang <zhangtiff@gmail.com>
2015-10-16 21:11:42 -0400

    Removed shadercache

---
+++ Milestone_2/Assets/ParticleScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleScript : MonoBehaviour {
+	public GameObject player;
+	public string particleState;
+
+	private Animator playerAnim;
+	private ParticleSystem particles;
+
+	// Use this for initialization
+	void Start () {
+		playerAnim = player.GetComponent<Animator> ();
+		particles = GetComponent<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerAnim.GetCurrentAnimatorStateInfo (0).IsName (particleState)) {
+			particles.enableEmission = true;
+		} else {
+			particles.enableEmission = false;
+		}
+		print (particles.enableEmission);
+	}
+}
