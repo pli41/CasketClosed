e09addf460ddc5151ebb095202a996ffdb1ccb30
charvi93 <charvi_5@yahoo.co.in>
2015-11-06 15:05:27 -0500

    position prediction worksgit add --allgit add --all yay tiffanygit add --allgit add --all

---
+++ Assets/Scripts/SpiderPositionPrediction.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderPositionPrediction : MonoBehaviour {
+	GameObject spider;
+	// Use this for initialization
+	void Start () {
+		spider = GameObject.Find("spider");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 addVector = spider.transform.forward;
+		Vector3 predictedPosition = spider.transform.position + addVector * 3;
+		gameObject.transform.position = predictedPosition;
+	}
+}
