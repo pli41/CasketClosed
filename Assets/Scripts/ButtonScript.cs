dc6a0d13f21013d0783e2a23609ee48383310894
Garrett Stache <gstache2@gmail.com>
2015-11-08 09:38:25 -0500

    removed water assets, added start screen w/ fade

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+    public Image panel;
+    public Color panelColor;
+	// Use this for initialization
+    void Start()
+    {
+        panelColor = panel.color;
+    }
+
+    void Update()
+    {
+        if (panel.color.a >= 1)
+        {
+            Application.LoadLevel("Room_for_Wake");
+        }
+        if (panel.enabled)
+        {
+            float fade = .5f;
+            panel.color = new Color(255, 255, 255, panel.color.a + Time.deltaTime * fade);
+            panelColor = panel.color;
+      
+        }
+       
+    }
+    public void OnClick()
+    {
+        panel.enabled = true;
+        
+       
+
+    }
+}
---
+++ Assets/Scripts/CameraOrbit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraOrbit : MonoBehaviour {
+    public GameObject target;
+    float radius;
+	// Use this for initialization
+	void Start () {
+        transform.LookAt(target.transform);
+        radius = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Mathf.Sin(Time.time/2) * radius, 2, Mathf.Cos(Time.time/2) * radius-2);
+        transform.LookAt(target.transform);
+	}
+}
