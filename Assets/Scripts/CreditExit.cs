3e8b8906b8ae08550daa7877677d2301d3c2ebb4
Garrett Stache <gstache2@gmail.com>
2015-11-23 10:25:05 -0500

    Animated Credits

---
+++ Assets/Scripts/CreditExit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditExit : MonoBehaviour {
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.LoadLevel("StartScreen");
+        }
+	}
+
+    public void Exit()
+    {
+        Application.LoadLevel("StartScreen");
+    }
+}
