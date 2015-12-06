ef0f7677f42c60716243cbe836f94a4bc0221f81
Tiff Zhang <zhangtiff@gmail.com>
2015-12-06 04:39:40 -0500

    Made a simple pause state and added a sound effect for getting caught by death.

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -1,17 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(AudioSource))]
 public class Death : MonoBehaviour {
+	public AudioClip caughtByDeath;
+	GameManager game;
+	AudioSource audio;
+
+	void Start() {
+		GameObject gm = GameObject.Find("GameManager");
+		game = gm.GetComponent<GameManager>();
+		audio = GetComponent<AudioSource>();
+	}
 
 	void OnTriggerEnter(Collider target)
     {
         if (target.gameObject.tag.Equals("Player"))
         {
-            GameObject gm = GameObject.Find("GameManager");
-            GameManager game = gm.GetComponent<GameManager>();
-            float totalTime = game.totalTime;
-            Application.LoadLevel("DeathScreen");
-            PlayerPrefs.SetFloat("time", totalTime);
+			CameraFollow camera = GameObject.Find("Main Camera").GetComponent<CameraFollow>();
+			camera.player = transform;
+
+			game.flashText("You've been caught by death. ", 2000);
+
+			game.PauseGame();
+
+			PlaySoundWithCallback(caughtByDeath, Die);
         }
     }
+
+	public delegate void AudioCallback();
+
+	void Die() {
+		game.UnpauseGame();
+		float totalTime = game.totalTime;
+		Application.LoadLevel("DeathScreen");
+		PlayerPrefs.SetFloat("time", totalTime);
+	}
+
+	public void PlaySoundWithCallback(AudioClip clip, AudioCallback callback)
+	{
+		audio.PlayOneShot(clip);
+		StartCoroutine(DelayedCallback(clip.length, callback));
+	}
+
+	private IEnumerator DelayedCallback(float time, AudioCallback callback)
+	{
+		yield return new WaitForSeconds(time);
+		callback();
+	}
 }
